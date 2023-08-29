562cef737f595c847d95c76b5212bf35e097a3a6
Ajay R <ajaynano.ajay7@gmail.com>
2023-08-29 15:13:48 +0530

    added destroy ec2 configrations

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -27,9 +27,39 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
+resource "aws_security_group" "launch-wizard-1" {
+  name = "security group from terraform"
+
+  ingress {
+    description = "SSH from the internet"
+    name = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "80 from the internet"
+    name = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.launch-wizard-1.id]
+  associate_public_ip_address = true
   key_name      = "app-ssh-key"
 
   tags = {
